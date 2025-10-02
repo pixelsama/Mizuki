a383c4e93f3fb5597454e27af3dd99e153d80940
pixelsama <pixelsama64@gmail.com>
2025-10-02 13:30:14 +0800

    update: 更新blog的左上角title

--- src/config.ts
+++ src/config.ts
@@ -21,8 +21,8 @@ import { getTranslateLanguageFromConfig } from "./utils/language-utils";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
-	title: "Mizuki",
-	subtitle: "One demo website",
+	title: "Pixelsama",
+	subtitle: "A beautiful blog",
 
 	lang: SITE_LANG,
 
